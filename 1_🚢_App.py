bc6537de5dac1656ba44086907fef97527249287
Nguyen Anh <nngguyen.anh@gmail.com>
2023-01-11 19:38:20 +0700

    VNH-9 | Answer and benefits of knowing price correlations

--- 1_🚢_App.py
+++ 1_🚢_App.py
@@ -1,2 +1,13 @@
 import streamlit as st
+from src.session.index import get_dataset
 
+st.title("Viet Nam Housing")
+
+st.title("Viet Nam Housing")
+
+st.header("Dataset")
+st.write("Giá nhà tại khu vực Hà Nội cung cấp bởi Kaggle.")
+
+df = get_dataset().copy()
+
+st.dataframe(df)

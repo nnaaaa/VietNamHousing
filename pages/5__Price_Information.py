df14d0764367118e95cc6bf42805f1a734bf2298
phanhuy1 <75444611+phanhuy1@users.noreply.github.com>
2023-01-11 17:16:50 +0700

    Update 5_price_information

--- pages/5__Price_Information.py
+++ pages/5__Price_Information.py
@@ -8,19 +8,25 @@ st.set_page_config(
     layout="wide",
 )
 
-df = get_data()
+data = get_data()
+le = LabelEncoder()
+data["district_label"] = le.fit_transform(data["district"])
+data["house_type_label"] = le.fit_transform(data["house_type"])
+data["town_label"] = le.fit_transform(data["town"])
 st.title("Viet Nam Housing")
 st.header("Dataset")
-st.write(df)
+st.write(data)
 
 st.markdown("***")
 st.header("Danh sách tất cả các quận:")
-district_list = list(set(df["district"]))
+district_list = data[["district","district_label"]]
+print(district_list)
 st.markdown(district_list)
 
 st.markdown("***")
 st.header("Danh sách tất cả loại nhà:")
 st.text('Nhà biệt thự: 0\nNhà mặt phố, mặt tiền: 1\nNhà ngõ, hẻm: 2\nNhà phố liền kề: 3\nKhông rõ: 4\n')
-st.markdown(sorted(df["house_type"].unique()))
+st.markdown(sorted(data['house_type'].unique()))
+
 #df['Range Price'] = pd.cut(x=df['price_per_m2'], bins=[0, 50000])
 # st.write(df[df[""]])

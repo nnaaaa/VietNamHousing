e694a57c269f1426673719889e173a2cdcaf17aa
Nguyen Anh <nngguyen.anh@gmail.com>
2023-01-11 16:47:54 +0700

    VNH-8 | Rename files

--- src/session/index.py
+++ src/session/index.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 @st.experimental_singleton
-def get_data():
+def get_dataset():
     df = pd.read_csv("data/processed/VN_housing_dataset.csv")
     
     return df
